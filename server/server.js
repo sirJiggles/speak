5a846505e4b6084c15fdfb060595e02bf514be78
Gareth Fuller <gareth-fuller@hotmail.co.uk>
2018-09-12 23:00:50 +0200

    allows cors on the server

--- server/server.js
+++ server/server.js
@@ -6,6 +6,16 @@ const bodyParser = require('body-parser');
 
 app.use(express.static('public'));
 
+// ALLOW CORS, FOR NOW
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  );
+  next();
+});
+
 app.use(bodyParser.json()); // support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 
